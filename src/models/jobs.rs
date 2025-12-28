52ffab30504d9f838501f9af4c7e8c6c32f78caf
miguelabdonsh <miguelabdonsh@gmail.com>
2025-12-28 18:21:39 +0100

    Add complete Job model with task type and payload support

--- src/models/jobs.rs
+++ src/models/jobs.rs
@@ -1,46 +1,81 @@
 use uuid::Uuid;
 use axum::{response::IntoResponse, Json};
-use serde::Serialize;
+use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use axum::extract::{State, Path};
 
+#[derive(Serialize)]
+pub struct JobResponse {
+    pub id: Uuid,
+    pub status: String,
+    pub task_type: Option<String>,
+    pub payload: Option<String>,
+}
+
+
+#[derive(Deserialize)]
+pub struct CreateJobRequest {
+    pub task_type: String,
+    pub payload: String,
+}
+
+#[derive(Clone)]
+pub struct Job {
+    pub id: Uuid,
+    pub status: String,
+    pub task_type: String,
+    pub payload: String,
+}
 
 #[derive(Clone)]
 pub struct AppState{
-    pub jobs: Arc<Mutex<HashMap<Uuid, String>>>,
+    pub jobs: Arc<Mutex<HashMap<Uuid, Job>>>,
 }
 
-
-#[derive(Serialize)]
-struct JobResponse {
-    id: Uuid,
-    status: String,
-}
-
-pub async fn create_job(State(state): State<AppState>) -> impl IntoResponse {
+pub async fn create_job(
+    State(state): State<AppState>,
+    Json(input): Json<CreateJobRequest>,
+) -> impl IntoResponse {
     let job_id = Uuid::new_v4();
-    let response = JobResponse {
+
+    let job = Job {
         id: job_id,
         status: "Pending".to_string(),
+        task_type: input.task_type,
+        payload: input.payload,
     };
 
-    state.jobs.lock().await.insert(job_id, "Pending".to_string());
+    state.jobs.lock().await.insert(job_id, job.clone());
+
+    let response = JobResponse {
+        id: job.id,
+        status: job.status,
+        task_type: Some(job.task_type),
+        payload: Some(job.payload),
+    };
 
     return Json(response);
 }
 
-pub async fn get_job_status(Path(job_id): Path<Uuid>, State(state): State<AppState>) -> impl IntoResponse {
+
+pub async fn get_job_status(
+    Path(job_id): Path<Uuid>,
+    State(state): State<AppState>,
+) -> impl IntoResponse {
     let jobs = state.jobs.lock().await;
-    if let Some(job) = jobs.get(&job_id){
+
+    if let Some(job) = jobs.get(&job_id) {
         let response = JobResponse {
-            id: job_id,
-            status: job.clone(),
+            id: job.id,
+            status: job.status.clone(),
+            task_type: Some(job.task_type.clone()),
+            payload: Some(job.payload.clone()),
         };
+
         return Json(response).into_response();
     } else {
         return (axum::http::StatusCode::NOT_FOUND, "Job not found").into_response();
     }
-
 }
